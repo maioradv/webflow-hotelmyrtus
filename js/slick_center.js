4b3ee878ea5fd4a80d7c4fe6291a4c09d6a177ae
maioradv <48984465+maioradv@users.noreply.github.com>
2022-12-01 15:04:26 +0100

    Update slick_center.js

--- js/slick_center.js
+++ js/slick_center.js
@@ -1,32 +1,34 @@
-	$(document).ready(function() {
-    $('.slick_center').slick({
-      dots: true,
-      infinite: true,
-      arrows: true,
-      centerMode: true,
-      centerPadding: '60px',
-      slidesToShow: 3,
-      draggable: false,
-            swipe: true,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
+$(document).ready(function() {
+	if($('.slick_center_items').length > 3){ 
+		$('.slick_center').slick({
+			dots: true,
+			infinite: true,
+			arrows: true,
+			centerMode: true,
+			centerPadding: '60px',
+			slidesToShow: 3,
+			draggable: false,
+			    swipe: true,
+			responsive: [
+			{
+			  breakpoint: 768,
+			  settings: {
+			    arrows: false,
+			    centerMode: true,
+			    centerPadding: '40px',
+			    slidesToShow: 3
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+			    arrows: false,
+			    centerMode: true,
+			    centerPadding: '40px',
+			    slidesToShow: 1
+			  }
+			}
+			]
+		});
+	}
 });
